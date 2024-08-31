3a7b71ce7bf87b367b9f31efb68ffd718ffe1d9f
Bilal Mirje <mirjebilal7@gmail.com>
2024-08-31 15:32:50 +0530

    added landing page

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 function Header() {
   return (
+    <div className='p-3 shadow-sm flex justify-between items-center px-5 w-full gap-[1380px]'>
+    <img src="/logo.svg" alt="Logo"/>
     <div>
-        <img className='max-w-[80px] p-2 shadow-sm' src="src\assets\images\logo.jpg" alt="" />
-        <div>
-            Butt
-        </div>
+        <Button>Sign In</Button>
     </div>
+  </div>
+
+
   )
 }
 
--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className='flex flex-col items-center mx-56 gap-9'>
+      <h1 className='font-extrabold text-[60px] text-center mt-16'>Welcome to the world of AI: <span className='text-[#f56551]'>you can find the best Plan for travel <br /></span></h1>
+      <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom intineries tailored to your interest and budgets.</p>
+      <Button>Get Started</Button>
+    </div>
   )
 }
 
