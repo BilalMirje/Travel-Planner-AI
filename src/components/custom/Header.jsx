931434a5da28226572212880ec76427bf858de82
Bilal Mirje <mirjebilal7@gmail.com>
2024-08-31 14:29:10 +0530

    landing page is added

---
+++ src/components/custom/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <img className='max-w-[80px] p-2 shadow-sm' src="src\assets\images\logo.jpg" alt="" />
+        <div>
+            Butt
+        </div>
+    </div>
+  )
+}
+
+export default Header
