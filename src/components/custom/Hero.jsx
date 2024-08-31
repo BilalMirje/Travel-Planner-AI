8a7bae81ceff589534926b35979ae829aad7d8e2
Bilal Mirje <mirjebilal7@gmail.com>
2024-08-31 16:43:55 +0530

    location api added

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import { Button } from '../ui/button'
+import React from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
-function Hero() {
+function CreateTrip() {
   return (
-    <div className='flex flex-col items-center mx-56 gap-9'>
-      <h1 className='font-extrabold text-[60px] text-center mt-16'>Welcome to the world of AI: <span className='text-[#f56551]'>you can find the best Plan for travel <br /></span></h1>
-      <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom intineries tailored to your interest and budgets.</p>
-      <Button>Get Started</Button>
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
+      <h2 className="font-bold text-3xl">Tell us your travel preferences</h2>
+      <p className="mt-3 text-gray-500 text-xl">
+        Just provide some basic information, and your trip planner will generate
+        a customized itinerary based on your preferences
+      </p>
+
+      <div className="mt-20">
+        <div>
+          <h2 className="text-xl my-3 font-medium">
+            What is your destination of choice?
+          </h2>
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+            selectProps={{
+              placeholder: "Search destinations...",
+            }}
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default CreateTrip;
